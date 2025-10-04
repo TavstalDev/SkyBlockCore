7ddbeddd3ba182b9dc741e5b5f35ba6e4e083495
tavstal <tavstal@gmail.com>
2025-10-04 15:57:48 +0200

    Add join time tracking for players

--- src/main/java/io/github/tavstaldev/skyBlockCore/managers/PlayerCacheManager.java
+++ src/main/java/io/github/tavstaldev/skyBlockCore/managers/PlayerCacheManager.java
@@ -7,6 +7,7 @@ import java.util.*;
 
 public class PlayerCacheManager {
     private static final Map<UUID, LocalDateTime> _playersInAfkPond = new HashMap<>();
+    private static final Map<UUID, LocalDateTime> _playersJoinTime = new HashMap<>();
 
     //#region Protections
     public static void addToAfkPond(UUID playerId, LocalDateTime time) {
@@ -23,6 +24,24 @@ public class PlayerCacheManager {
 
     public static Map<UUID, LocalDateTime> getPlayersInAfkPond() {
         return Collections.unmodifiableMap(_playersInAfkPond);
+    }
+    //#endregion
+
+    //#region Join Time
+    public static void addJoinTime(UUID playerId, LocalDateTime time) {
+        _playersJoinTime.put(playerId, time);
+    }
+
+    public static void removeJoinTime(UUID playerId) {
+        _playersJoinTime.remove(playerId);
+    }
+
+    public static @Nullable LocalDateTime getJoinTime(UUID playerId) {
+        return _playersJoinTime.get(playerId);
+    }
+
+    public static Map<UUID, LocalDateTime> getPlayersJoinTime() {
+        return Collections.unmodifiableMap(_playersJoinTime);
     }
     //#endregion
 }
